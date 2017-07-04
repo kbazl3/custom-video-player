7a8d5ecef06a22f18d89e7f1f249c1ac681a7173
Kyle Barlow <kbarlow@kbarlow-l2.corp.instructure.com>
2017-07-04 00:19:34 -0600

    added volume control and playback rate

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ const progressBar = player.querySelector('.progress__filled');
 
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 //build functions
 
@@ -34,15 +34,52 @@ function skip() {
     video.currentTime += parseInt(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.name);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    console.log(e);
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 //hook up event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
 
 skipButtons.forEach(function(button) {
     button.addEventListener('click', skip);
+});
+
+ranges.forEach(function(button) {
+    button.addEventListener('change', handleRangeUpdate);
+});
+
+ranges.forEach(function(button) {
+    button.addEventListener('mousemove', handleRangeUpdate);
+});
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', function(e) {
+    if (mousedown && scrub(e));
+});
+progress.addEventListener('mousedown', function() {
+    mousedown = true;
+});
+progress.addEventListener('mouseup', function() {
+    mousedown = false;
 });
